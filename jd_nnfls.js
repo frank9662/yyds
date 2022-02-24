12d39f84859cc060278de083c69aa5281bbed349
a00126 <87116510+a00126@users.noreply.github.com>
2022-02-24 10:46:24 +0800

    Update jd_nnfls.js

--- jd_nnfls.js
+++ jd_nnfls.js
@@ -54,7 +54,7 @@ if ($.isNode()) {
     }
     shareCodes = shareCodes.filter(code => code)
     const author = Math.random() > 0.5 ? 'zero205' : 'ZXX2021'
-    await getShareCode('nnfls.json', author, 3, true)
+    await getShareCode('', author, 3, true)
     shareCodes = [...new Set([...shareCodes, ...($.shareCode || [])])];
     if (shareCodes.length > 0) {
         console.log(`\n*********开始互助**********\n`);
@@ -90,7 +90,7 @@ if ($.isNode()) {
 
 })().catch((e) => { $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '') }).finally(() => { $.done(); })
 
-function getShareCode(name, author = 'zero205', num = -1, shuffle = false) {
+function getShareCode(name, author = '', num = -1, shuffle = false) {
     return new Promise(resolve => {
         $.get({
             url: ``,
